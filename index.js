2dac4ff789015f68c1dabaeb7ea5b03bc8cb6c35
Conor Clark <cclark509@gmail.com>
2018-06-29 15:06:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function logWhisper(string) {
 
 
 function  sayHiToGrandma(string) {
-  if (string.toLowerCase())
-  return "I can't hear you"
+  if (string.toLowerCase()) 
+  return "I can't hear you."
 }
 {
   if (string.toUpperCase())
