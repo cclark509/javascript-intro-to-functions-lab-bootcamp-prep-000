b9782b7e7ea5d5684ab3b034a2508435d5021c46
Conor Clark <cclark509@gmail.com>
2018-06-29 15:00:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,5 @@ function  sayHiToGrandma(string) {
 }
 
 function  sayHiToGrandma(string) {
-  if (string('I love you, Grandma')
-  return "I love you, too."
+  console.log('I love you, Grandma')
 }
