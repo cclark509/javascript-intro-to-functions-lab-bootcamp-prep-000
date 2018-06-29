b444f812331a201aaf35fbe3b8c38652bddf897b
Conor Clark <cclark509@gmail.com>
2018-06-29 14:53:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+var mixedCase = 'I love you, Grandma.'
+
+function  sayHiToGrandma(string) {
+  if (string.toLowerCase())
+  return "I can't hear you"
+}
+function  sayHiToGrandma(string) {
+  if (string.toUpperCase())
+  return "YES INDEED!"
+}
+function  sayHiToGrandma(string) {
+  console.log(string.mixedCase())
+  return "I love you, too."
+}
